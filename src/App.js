54f3ca2769162c7c90c8e4027faf65451a1ee447
Kirill Borisiuk <kirilluborisyukal03@gmail.com>
2024-07-10 16:19:34 +0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {details: [], }
+
+  componentDidMount() {
+    let data;
+    axios.get("http://localhost:8000")
+        .then(res => {
+          data = res.data;
+          this.setState({
+            details: data
+          });
+        })
+        .catch(err=> {
+          console.log(err)
+        })
+  }
+
+  render() {
+    return(
+        <div>
+          <header>
+            КНИГИ!
+          </header>
+          <hr></hr>
+          {this.state.details.map((output, id) => (
+              <div key={id}>
+                <div>
+                  <h2>{output.name}</h2>
+                  <h2>{output.author}</h2>
+                  <h2>{output.year}</h2>
+                  <h2>{output.rating}</h2>
+                  <h2>{output.isbn}</h2>
+                  <h2>{output.quantity}</h2>
+                  <h2>{output.tags}</h2>
+                  <hr></hr>
+                </div>
+              </div>
+          ))}
+        </div>
+    )
+  }
 }
-
 export default App;
